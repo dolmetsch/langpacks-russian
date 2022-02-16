bf5b76b0d7f8207ece09d06b72e64b54dc0752d2
gbezyuk <dev@gbezyuk.ru>
2022-02-16 18:18:46 +0300

    alphabet -> characters rename

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Learning or analyzing Russian? Feel free to use this data as a reference.
 
 ## Contents
 * `characters.json` — condensed "all in one" list of Russian characters. Lowercase-based.
-* `alphabet/full.json` — ordered list of letters of Russian alphabet. All lowercase.
-* `alphabet/subsets/..` — subsets of the alphabet: consonants, vowels, other characters. All lowercase.
+* `characters/alphabet.json` — ordered list of letters of Russian alphabet. All lowercase.
+* `characters/subsets/..` — subsets of the alphabet: consonants, vowels, other characters. All lowercase.
 * `maps/frequencies.json` — dictionary of occurence frequencies for Russian alphabet. All lowercase.
 * `maps/uppercase.json` — lowercase to uppercase map.
 * `keyboard_layouts/..` — set of full maps from QWERTY to Cyrillic layouts.
--- _scripts/zipCharacters.mjs
+++ _scripts/zipCharacters.mjs
@@ -2,15 +2,15 @@ import readJSON from './utils/readJSON.mjs'
 import zipAlphabetCharacters from './utils/zipAlphabetCharacters.mjs'
 
 
-const alphabet = readJSON('../alphabet/full.json')
+const alphabet = readJSON('../characters/alphabet.json')
 const layout = readJSON('../keyboard_layouts/macos-default.inverse.json')
 const frequencies = readJSON('../maps/frequencies.json')
 const uppercases = readJSON('../maps/uppercase.json')
 
 const subsets = {
-    'vowel': readJSON('../alphabet/subsets/vowels.json'),
-    'consonant': readJSON('../alphabet/subsets/consonants.json'),
-    'other': readJSON('../alphabet/subsets/other.json'),
+    'vowel': readJSON('../characters/subsets/vowels.json'),
+    'consonant': readJSON('../characters/subsets/consonants.json'),
+    'other': readJSON('../characters/subsets/other.json'),
 }
 
 
